f9c2e04f91502bf784357a0724a6a75f8160836b
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2024-08-02 21:00:33 +0300

    добавлен интерфейс

--- app/src/main/java/com/arziman_off/getrandomjoke/MainActivity.java
+++ app/src/main/java/com/arziman_off/getrandomjoke/MainActivity.java
@@ -2,25 +2,47 @@ package com.arziman_off.getrandomjoke;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "MainActivity";
     private MainViewModel viewModel;
+    private MaterialButton btnNewGenerate;
+    private TextView tvOneJokeSetup;
+    private TextView tvOneJokePunchline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         viewModel.loadOneNewJoke();
         viewModel.getJokeItem().observe(this, new Observer<JokeItemInfo>() {
             @Override
             public void onChanged(JokeItemInfo jokeItem) {
+                tvOneJokeSetup.setText(jokeItem.getSetup());
+                tvOneJokePunchline.setText(jokeItem.getPunchline());
                 Log.d(LOG_TAG, jokeItem.toString());
             }
         });
+        btnNewGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.loadOneNewJoke();
+            }
+        });
+    }
+
+    private void initViews() {
+        btnNewGenerate = findViewById(R.id.btnNewGenerate);
+        tvOneJokeSetup = findViewById(R.id.tvOneJokeSetup);
+        tvOneJokePunchline = findViewById(R.id.tvOneJokePunchline);
     }
 }
